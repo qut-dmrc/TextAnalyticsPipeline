fd1bc014a8e234a702b12dd6c0ae3019cd125d22
vodden <laura.vodden@outlook.com>
2025-06-03 11:51:00 +1000

    update requirements.txt and add module downloads to nltk_pipe.py

--- TextAnalyticsPipeline/nltk_pipe.py
+++ TextAnalyticsPipeline/nltk_pipe.py
@@ -10,6 +10,9 @@ nltk.download('punkt')
 nltk.download('averaged_perceptron_tagger')
 nltk.download('maxent_ne_chunker')
 nltk.download('words')
+nltk.download('punkt_tab')
+nltk.download('averaged_perceptron_tagger_eng')
+nltk.download('maxent_ne_chunker_tab')
 
 
 
